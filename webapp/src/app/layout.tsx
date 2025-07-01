27b25d2a972b8a31d9389ccdd27bb63bf24859f9
am-2112 <a.mclauchlan2112@gmail.com>
2025-07-01 11:41:26 +0100

    Add files via upload

---
+++ webapp/src/app/layout.tsx
@@ -0,0 +1,25 @@
+import '@/app/styles/global.css';
+
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function RootLayout({
+    children,
+}: {
+        children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <div className="toolbar sticky">
+                    <Image src="/favicon.ico" alt="img" width="50" height="50"></Image>
+                    <nav className="spaced self-center">
+                        <Link id="Home Link" className="spaced" href="/library">Home</Link>
+                        <Link id="Library Link" className="spaced" href="/library">Library</Link>
+                    </nav>
+                </div>
+                {children}
+            </body>
+        </html>
+    )
+}
---
+++ webapp/src/app/page.tsx
@@ -0,0 +1,8 @@
+﻿
+export default function Page() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
